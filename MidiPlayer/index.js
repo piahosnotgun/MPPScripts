40e5c0526b8b7b121500626c556a5e5cecc6d58e
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-08-17 06:13:18 +0000

    MidiPlayer 삭제 이거 쓸 필요가 없음

--- MidiPlayer/index.js
+++
@@ -1,8 +0,0 @@
-loadParser().then(()=>{
-	let parser = MidiParser;
-});
-async function loadParser(){
-	let res = await fetch('https://colxi.info/midi-parser-js/src/main.js');
-	let text = await res.text;
-	eval(text);
-}
