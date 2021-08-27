8ce2ab067e90dfd11a4a89167dfbca65db9e92e9
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-08-27 14:11:27 +0000

    이상한짓 고침

--- BanUtils/index.js
+++ BanUtils/index.js
@@ -16,45 +16,44 @@ MPP.client.on('a', (data) => {
             }
             chat.send('사용자 아이디 ' + args[0] + ' (이)가 차단되었습니다.');
             ban(args[0]);
+        } else if (cmd === '/anonban') {
+            if (anonban) {
+                chat.send('익명 유저 차단이 해제되었습니다.');
+            } else {
+                chat.send('익명 유저를 차단합니다.');
+            }
+            anonban = !anonban;
+        } else if (cmd === '/pardon') {
+            if (typeof args[0] === 'undefined') {
+                chat.send('차단 해제할 유저의 id를 입력해주세요');
+                return;
+            }
+            if (typeof banlist[args[0]] !== 'undefined') {
+                delete banlist[args[0]];
+                chat.send('차단 해제가 완료되었습니다.');
+                return;
+            } else {
+                chat.send('잘못된 아이디입니다. 아이디를 확인하신 후 다시 시도해주세요.');
+            }
+        } else if (cmd === 'banlist') {
+            let txt = '금지된 유저 목록: ';
+            for (let banned in banlist) {
+                txt += banned + ' ';
+            }
+            chat.send(txt);
         }
-    } else if (cmd === '/anonban') {
-        if (anonban) {
-            chat.send('익명 유저 차단이 해제되었습니다.');
-        } else {
-            chat.send('익명 유저를 차단합니다.');
-        }
-        anonban = !anonban;
-    } else if (cmd === '/pardon') {
-        if (typeof args[0] === 'undefined') {
-            chat.send('차단 해제할 유저의 id를 입력해주세요');
-            return;
-        }
-        if (typeof banlist[args[0]] !== 'undefined') {
-            delete banlist[args[0]];
-            chat.send('차단 해제가 완료되었습니다.');
-            return;
-        } else {
-            chat.send('잘못된 아이디입니다. 아이디를 확인하신 후 다시 시도해주세요.');
-        }
-    } else if (cmd === 'banlist') {
-        let txt = '금지된 유저 목록: ';
-        for (let banned in banlist) {
-            txt += banned + ' ';
-        }
-        chat.send(txt);
     }
     if (cmd === '/list') {
-		let names = $('.nametext');
-		let list = "";
-		for(let n in names){
-			let element = names[n];
-			let id = element.id;
-			if(typeof id === 'undefined')
-				continue;
-			let name = element.innerText;
-			list += name + "(" + id.substring(9) + ") ";
-		}
-		chat.send(list);
+        let names = $('.nametext');
+        let list = '';
+        for (let n in names) {
+            let element = names[n];
+            let id = element.id;
+            if (typeof id === 'undefined') continue;
+            let name = element.innerText;
+            list += name + '(' + id.substring(9) + ') ';
+        }
+        chat.send(list);
     }
 });
 MPP.client.on('participant update', (p) => {
